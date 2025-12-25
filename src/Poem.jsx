cd160481dec231696cc901cf6a5ca64528332ea6
Kosei Oki <koseioki1225@gmail.com>
2025-12-25 15:36:27 +0100

    disabled poem link

--- src/Poem.jsx
+++ src/Poem.jsx
@@ -5,14 +5,12 @@ export function Poem({ id, lines }) {
     return (
         <li>
             <NavLink to={`/poem/${id}`} className="poem-item">
-                {/* <p> */}
                     {lines.map((line, index) => (
                         <span key={`${line}-${index}`}>
                             {line}
                             {index < lines.length - 1 && <br />}
                         </span>
                     ))}
-                {/* </p> */}
             </NavLink>
 
         </li>
--- src/PoemDetail.jsx
+++ src/PoemDetail.jsx
@@ -4,24 +4,31 @@ import { Poem } from './Poem'
 
 export default function PoemDetail() {
 
-  const { id } = useParams()
-  const poem = poems.find((entry) => entry.id === id)
+    const { id } = useParams()
+    const poem = poems.find((entry) => entry.id === id)
 
-  if (!poem) {
+    if (!poem) {
+        return (
+            <section className="poem-container">
+                <Link to="/" className="back-link">Back to list</Link>
+                <p>Sorry, that poem was not found.</p>
+            </section>
+        )
+    }
+
     return (
-      <section className="poem-container">
-        <Link to="/" className="back-link">Back to list</Link>
-        <p>Sorry, that poem was not found.</p>
-      </section>
-    )
-  }
+        <section className="poem-container">
+            <Link to="/" className="back-link">Back to list</Link>
+            {/* <ul className="poem-list"> */}
+            {/* <Poem id={poem.id} lines={poem.lines} /> */}
+            {/* </ul> */}
 
-  return (
-    <section className="poem-container">
-      <Link to="/" className="back-link">Back to list</Link>
-      <ul className="poem-list">
-        <Poem id={poem.id} lines={poem.lines} />
-      </ul>
-    </section>
-  )
+            {poem.lines.map((line, index) => (
+                <span key={`${line}-${index}`}>
+                    {line}
+                    {index < poem.lines.length - 1 && <br />}
+                </span>
+            ))}
+        </section>
+    )
 }
